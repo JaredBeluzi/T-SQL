d2ea8e5d91cc999ab5ac4507175b1666751ac76c
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2023-12-22 10:25:23 +0100

    Create CREATE TABLE.sql

---
+++ collection/CREATE TABLE.sql
@@ -0,0 +1,17 @@
+-- Example
+
+CREATE TABLE dbo.fruits (
+    name VarChar(100)
+,   price Float
+,   primary_color VarChar(20)
+)
+
+
+-- General
+
+CREATE TABLE table_name (
+    column1 datatype
+,   column2 datatype
+,   column3 datatype
+   ....
+)
