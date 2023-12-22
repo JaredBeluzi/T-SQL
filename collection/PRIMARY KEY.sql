3747ee5480b2f8621b160e0a62d1d99ee937e7b4
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2023-12-22 11:09:33 +0100

    Update PRIMARY KEY.sql

--- collection/PRIMARY KEY.sql
+++ collection/PRIMARY KEY.sql
@@ -1,9 +1,17 @@
--- Example
+-- 0. What does it do?
+
+/*
+This SQL creates a Primary Key on a table.
+*/
+
+
+-- 1. Example
 
 ALTER TABLE dbo.fruits
 ADD PRIMARY KEY (name)
 
--- General
+  
+-- 2. General
 
 ALTER TABLE table_name
 ADD PRIMARY KEY (column_name)
