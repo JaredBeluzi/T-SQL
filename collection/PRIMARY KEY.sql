022a09d3e7ed4e1a5c58950b0724aefcd4692ac9
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2023-12-22 14:35:20 +0100

    Added hint for columns with NULL values
    Added hint for what to do when column is allowed to have NULL values, which gives an error message

--- collection/PRIMARY KEY.sql
+++ collection/PRIMARY KEY.sql
@@ -15,3 +15,12 @@ ADD PRIMARY KEY (name)
 
 ALTER TABLE table_name
 ADD PRIMARY KEY (column_name)
+
+
+-- 3. Hint
+
+-- It is necessary for column_name to not allow NULL values.
+-- You can do this with the following SQL
+
+ALTER TABLE table_name
+ALTER COLUMN column_name DataType NOT NULL -- DataType should be the same as before
