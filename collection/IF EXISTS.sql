fe5b774865756697dce471d09e8c472d7482ffd5
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-11-01 11:49:26 +0100

    minimalisieren und eindeutschen

--- collection/IF EXISTS.sql
+++ collection/IF EXISTS.sql
@@ -1,43 +1,17 @@
--- 0. What does it do?
-
 /*
-This SQL checks if a table (with a certain column) exists and only if that is the case, some SQL will be executed.
+Code prüft ob Tabelle ST_HelicDynamic (mit Spalte HelicDynamic_Status) existiert und führt nur dann SQL aus.
 */
-
-
--- 1. Example
 
 IF EXISTS(
 		SELECT o.* 
 		FROM 		      sys.objects AS o
-		INNER JOIN		sys.columns AS c -- remove join if you only want to check if a table exists
+		INNER JOIN		sys.columns AS c -- JOIN entfernen, wenn man nur prüfen will ob Tabelle an sich existiert
 			ON	o.object_ID = c.object_ID
  		WHERE o.Name = 'ST_HelicDynamic'
-		AND	  c.Name = 'HelicDynamic_Status') -- remove if you only want to check if a table exists
+		AND   c.Name = 'HelicDynamic_Status') -- ebenfalls entfernen, wenn man nur prüfen will ob Tabelle an sich existiert
 
 BEGIN
 
-/*
-Put SQL here, that shall be executed, if the table ST_HelicDynamic (with the column HelicDynamic_Status) exists.
-*/
-
-END
-
-  
--- 2. General
-
-IF EXISTS(
-		SELECT o.* 
-		FROM 		      sys.objects AS o
-		INNER JOIN		sys.columns AS c -- remove join if you only want to check if a table exists
-			ON	o.object_ID = c.object_ID
- 		WHERE o.Name = 'table_name'
-		AND	  c.Name = 'column_name') -- remove if you only want to check if a table exists
-
-BEGIN
-
-/*
-Put SQL here, that shall be executed, if the table table_name (with the column column_name) exists.
-*/
+-- Hier SQL schreiben, der dann ausgeführt werden soll
 
 END
