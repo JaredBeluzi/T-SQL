9e6eb0413b2545601af38d2aab2772e6131064d8
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-11-01 13:09:52 +0100

    minimalisieren und eindeutschen

--- collection/RANKING.sql
+++ collection/RANKING.sql
@@ -1,42 +1,33 @@
--- 0. What does it do?
 
-/*
-This SQL allows you to rank a column_1 for each value of a column_2 and returns only the values of column_1 with the highest rank.
-This concept can be relaxed if necessary.
-Application examples:
-- find the last address for each person
-- find the most important ingredient for each dish
-- find the last 3 recipients of payments for each customer in a bank
+-- Für jeden Wert in Spalte 1 die Werte in Spalte 2 ranken
+
+/* 
+Bsp: letzte Adresse einer Person finden
+Adresse
+Adresse_ID   Person_ID   Adresse_gültig_von   Adresse_gültig_bis   Adresse_Straße   Adresse_Hausnummer    RANKING
+1            1           01.01.2019           01.06.2019           Haupstraße       5                     4
+2            1           03.06.2019           01.01.2023           Haupstraße       4                     3
+3            1           01.01.2020           01.01.2023           Haupstraße       10                    2
+4            1           01.01.2020           01.01.2023           Haupstraße       50                    1		<-- Die Adresse hier würde man dann ausgeben für die Person
 */
 
-
--- 1. Example
-
 SELECT
-	SUB.Unternehmen_ID
-,	SUB.Unternehmen_Bezeichnung
-,	SUB.Person_ID
-,	SUB.Person_Vorname
-,	SUB.Person_Nachname
-INTO	dbo.längster_Mitarbeiter
+	SUB.Person_ID
+,	SUB.Adresse_Straße
+,	SUB.Adresse_Hausnummer
+INTO	dbo.Ergebnis
 FROM
 (
 SELECT 
-	Unternehmen.Unternehmen_ID
-	Unternehmen.Bezeichnung AS Unternehmen_Bezeichnung
-,	Unternehmen.Person_ID
-,	Person.Vorname AS Person_Vorname
-,	Person.Nachname AS Person_Nachname
-,	RANK() OVER ( PARTITION BY Unternehmen.Unternehmen_ID ORDER BY 	
-        	Person.Beitrittsdatum ASC
-	,	Person.Kündigungsdatum DESC
-	,	Person.Hierarchie DESC
-	,	Person.Person_ID DESC) AS RANKING
-	-- try to use as many meaningful columns to rank
-	-- as last step use a column that ensures the uniqueness of rank 1 (often an ID col)	
-FROM dbo.Unternehmen AS Unternehmen
-INNER JOIN	dbo.Person AS Person
-	ON	Unternehmen.Unternehmen_ID = Person.Unternehmen_ID
-WHERE Person.StornoKZ = 0
+	Person.Person_ID
+	Adresse.Adresse_Straße
+,	Adresse.Adresse_Hausnummer
+,	RANK() OVER ( PARTITION BY Person.Person_ID ORDER BY 	
+        	Adresse.Adresse_gültig_bis DESC
+	,	Adresse.Adresse_gültig_von DESC
+	,	Adresse.Adresse_ID DESC) AS RANKING -- als letzten Rankingfaktor ID nehmen, um Eindeutigkeit bei Datenmüll zu erzwingen
+FROM dbo.Person AS Person
+INNER JOIN dbo.Adresse AS Adresse
+    ON Person.Person_ID = Adresse.Person_ID
 ) AS SUB
 WHERE SUB.RANKING = 1
