f3ab8f0738d99361fe9eb3820ff9a4985e84d66c
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-11-01 11:44:35 +0100

    Code minimalisieren und eindeutschen

--- collection/CREATE TABLE.sql
+++ collection/CREATE TABLE.sql
@@ -1,23 +1,6 @@
--- 0. What does it do?
-/*
-This SQL creates an empty table with columns.
-*/
-
-
--- 1. Example
-
-CREATE TABLE dbo.fruits (
-    name VarChar(100)
-,   price Float
-,   primary_color VarChar(20)
-)
-
-
--- 2. General
-
-CREATE TABLE table_name (
-    column1 datatype
-,   column2 datatype
-,   column3 datatype
-   ....
+CREATE TABLE dbo.Personen (
+    Vorname VarChar(100)
+,   Nachname VarChar(100)
+,   Adresse_Straße VarChar(200)
+,   Adresse_Hausnummer Int
 )
