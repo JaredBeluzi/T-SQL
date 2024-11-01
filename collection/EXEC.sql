25e3a6cf89e01d5778d78e20c8a3d2abe8c8dbcb
Jared Beluzi <126320399+JaredBeluzi@users.noreply.github.com>
2024-11-01 11:46:13 +0100

    Code minimalisieren und eindeutschen

--- collection/EXEC.sql
+++ collection/EXEC.sql
@@ -1,29 +1,9 @@
--- 0. What does it do?
-
-/*
-This SQL shows how to execute text as SQL.
-*/
-
-
--- 1. Example
-
-DECLARE @sql VarChar(Max) -- create Text Variable
-SET @sql =  -- fill Text Variable with code, that you want to execute
+DECLARE @sql VarChar(Max) -- Textvariable erstellen
+SET @sql =  -- Textvariable mit Code füllen, der ausgeführt werden soll
 '
 SELECT *
 INTO dbo.ES_1010
 FROM dbo.TT_v
 '
 
-EXEC(@sql) -- execute code
-
-  
--- 2. General
-
-DECLARE @sql VarChar(Max) -- create Text Variable
-SET @sql =  -- fill Text Variable with code, that you want to execute
-'
-...
-'
-
-EXEC(@sql) -- execute code
+EXEC(@sql) -- Code in Textvariable ausführen
